afa1243e975e0dd05f05a319954d8f2f5883d5a9
mateustomaz1 <mateustomaz147@gmail.com>
2024-11-08 17:06:26 -0300

    comit

---
+++ src/test/java/com/ifpb/edu/couse/CouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifpb.edu.couse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CouseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
