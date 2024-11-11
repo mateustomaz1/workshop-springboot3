99ca3d37a8bd8d9435f07d8389cf4aa404ea5995
mateustomaz1 <mateustomaz147@gmail.com>
2024-11-11 16:23:00 -0300

    atualização test

---
+++ src/main/java/com/ifpb/edu/couse/repositories/UserRepository.java
@@ -0,0 +1,7 @@
+package com.ifpb.edu.couse.repositories;
+
+import com.ifpb.edu.couse.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
