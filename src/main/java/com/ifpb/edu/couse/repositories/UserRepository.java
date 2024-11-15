0218685b638eea18ce8523d12346539186e31d23
mateustomaz1 <mateustomaz147@gmail.com>
2024-11-15 14:56:19 -0300

    Service layer

--- src/main/java/com/ifpb/edu/couse/repositories/UserRepository.java
+++ src/main/java/com/ifpb/edu/couse/repositories/UserRepository.java
@@ -3,5 +3,6 @@ package com.ifpb.edu.couse.repositories;
 import com.ifpb.edu.couse.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface UserRepository extends JpaRepository<User, Long> {
 }
