67fcfb86134ab5c53c97b985768d05ce1b66fb00
Mateus Tomaz <mateustomaz147@gmail.com>
2025-01-11 14:44:12 -0300

    OrderStatus enum

--- src/main/java/com/ifpb/edu/couse/entities/Order.java
+++ src/main/java/com/ifpb/edu/couse/entities/Order.java
@@ -3,6 +3,7 @@ package com.ifpb.edu.couse.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ifpb.edu.couse.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -24,6 +25,17 @@ public class Order implements Serializable {
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if (orderStatus != null) {
+        this.orderStatus = orderStatus.getCode();
+        }
+    }
+
+    private Integer orderStatus;
 
     @ManyToOne
     @JoinColumn(name = "client_id")
@@ -31,9 +43,10 @@ public class Order implements Serializable {
 
     public Order() {}
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
